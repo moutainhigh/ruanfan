2b1329f70423742337f32f6adb1ee2c8bf42177e
Hastmer <412824651@qq.com>
2016-03-20 18:04:10 +0800

    修改商品新增界面

--- src/main/java/com/sixmac/controller/ProductController.java
+++ src/main/java/com/sixmac/controller/ProductController.java
@@ -16,11 +16,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartRequest;
 
-import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.*;
 
