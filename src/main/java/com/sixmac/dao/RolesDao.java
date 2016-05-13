f937e8ff55c6a09b3c420582a1ec65d243c2c5b6
Hastmer <412824651@qq.com>
2016-05-13 17:56:53 +0800

    增加积分

--- src/main/java/com/sixmac/dao/RolesDao.java
+++ src/main/java/com/sixmac/dao/RolesDao.java
@@ -1,17 +1,15 @@
 package com.sixmac.dao;
 
-import com.sixmac.entity.Reserve;
 import com.sixmac.entity.Roles;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 /**
  * Created by Administrator on 2016/3/4 0004 下午 2:47.
  */
-public interface RolesDao extends JpaRepository<Roles, Integer>, JpaSpecificationExecutor<Reserve> {
+public interface RolesDao extends JpaRepository<Roles, Integer> {
 
     @Query("select a from Roles a where a.id <> 1")
     public Page<Roles> page(Pageable pageable);
